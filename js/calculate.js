8c30e3d41637ffb36160f2f62deef683d652d4c4
DeveloperFahad <developerfahad.me@gmail.com>
2024-02-20 20:06:06 +0600

    Added validation for input field

--- js/calculate.js
+++ js/calculate.js
@@ -1,27 +1,36 @@
 function calculateTriangleArea(){
-    // const triangleBaseInput = document.getElementById('triangle-base');
-    // const triangleHeightInput = document.getElementById('triangle-height');
-    // if(triangleBaseInput.value !== 'number' || triangleHeightInput.value !== 'number' ){
-    //     console.log('Please enter a number');
-    // }
-    // if(!isNaN(triangleBaseInput.value))
-    
     
     const triangleBaseValue = document.getElementById('triangle-base').value;
     const base = parseFloat(triangleBaseValue);
-    if(typeof base !== 'number'){
-        console.log('Invalid');
-    }
     console.log(base);
     
     const triangleHeightValue = document.getElementById('triangle-height').value;
     const height = parseFloat(triangleHeightValue)
     console.log(height);
-    const area = 0.5 * base * height;
+
+    const totalArea = 0.5 * base * height;
+    const area = Math.sqrt(totalArea).toFixed(3);
+
     console.log("Total area is: ", area);
 
     const triangleAreaSpan = document.getElementById('triangle-area');
     triangleAreaSpan.innerText = area;
     // return area;
+    if(isNaN(base) && isNaN(height)){
+        alert("Your given Base and Height value is not correct.")
+    }
+    if(isNaN(base)){
+        alert("Please Enter the correct Base value.");
+    }
+    if(isNaN(height)){
+        alert("Plase Enter the correct height value.")
+    }
+    
+    if(base < 0 ){
+        alert("Base should be a positive number")
+    }
+    if(height < 0){
+        alert("Height should be a positive number")
+    }
 
 }
